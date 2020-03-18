eca17c086b9bbb71153f20134bd8741433d43787
jakob.a.modigh@gmail.com <jakob.a.modigh@gmail.com>
2020-03-18 10:56:20 +0100

    init

--- Uppgift2meny/Program.cs
+++ Uppgift2meny/Program.cs
@@ -11,13 +11,17 @@ namespace Uppgift2meny
             Console.WriteLine(@"Main menu
 0 för att avsluta
 1 för att beräkna dit pris
-2 för att beräkna priset för en grupp");
+2 för att beräkna priset för en grupp
+3 för att uprepa 10 gånger
+4 för att skriva ut de tredje ordet i en mening");
+
             
-            var menuOpt = Console.ReadLine();
             bool go = true;
 
             while (go)
             {
+                var menuOpt = Console.ReadLine();
+
                 switch (menuOpt)
                 {
                     case "0": // exit
@@ -32,10 +36,11 @@ namespace Uppgift2meny
                             Console.WriteLine("Ange en giltig siffra!");
                             break;
                         }
-
-                        if (oneage < 20) { Console.WriteLine("Ungdomspris: 80 kr"); }
-                        else if (oneage > 65) { Console.WriteLine("Pensionärspris: 90 kr"); }
-                        else { Console.WriteLine("Standardpris: 120 kr"); }
+                        if (oneage < 5) { Console.WriteLine("Under 5 är gratis"); }
+                        else if (oneage < 20) { Console.WriteLine("Ungdomspris: 80 kr"); }
+                        else if (oneage < 65) { Console.WriteLine("´Standardpris: 120 kr"); }
+                        else if (oneage < 100){ Console.WriteLine("Pensionärspris: 120 kr"); }
+                        else { Console.WriteLine("Över 100 är gratis"); }
                         go = false;
                         break;
 
@@ -65,12 +70,38 @@ namespace Uppgift2meny
 
                         foreach (int age in ages)
                         {
-                            if (age < 20) { totPris += 80; }
-                            else if (age > 65) { totPris += 90; }
-                            else { totPris += 120; }
+                            if (age <= 20 && age >= 5) { totPris += 80; }
+                            else if (age <= 65 && age >= 20) { totPris += 120; }
+                            else if (age >= 65 && age <= 100) { totPris += 90; }
                         }
+                        if (totPris == 0) { Console.WriteLine("Under 5 och över 100 är gratis");}
                         Console.WriteLine("För " + size + " personer kostar det:" + totPris + "kr");
                         go = false;
+                        break;
+
+                    case "3": // Upprepa tio gånger
+                        Console.Write("Enter input line:");
+                        var input = Console.ReadLine();
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.Write(i + 1 + "." + input + ", ");
+                        }
+                        break;
+
+                    case "4": // Det tredje ordet
+                        Console.WriteLine("Ange en mening med minst tre ord:");
+                        int x = 0;
+                        do
+                        {
+                            var ord = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                            string[] orden = ord.ToArray();
+                            x = orden.Count();
+
+                            if (x < 3) { Console.WriteLine("Ange en mening med minst tre ord!"); }
+                            else { Console.WriteLine(orden[2]); }
+                            
+                        } while (x < 3);
                         break;
 
                     default: // Fel input
